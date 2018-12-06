ab0105b14bd82621209500d7b96728f5ebb9420b
ruisousa <rvcsousa@hotmail.com>
2018-12-06 17:20:49 -0100

    player move

--- Game/core/src/org/academiadecodigo/hashtronauts/characters/player/PlatformPlayer.java
+++ Game/core/src/org/academiadecodigo/hashtronauts/characters/player/PlatformPlayer.java
@@ -1,16 +1,57 @@
 package org.academiadecodigo.hashtronauts.characters.player;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 public class PlatformPlayer extends Player {
 
-    private void jump(){
+    private int x;
+    private int y;
+
+
+    public PlatformPlayer(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    private void jump() {
 
     }
 
-    private void move(){
+    private void move() {
+
+
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            x += 200 * Gdx.graphics.getDeltaTime();
+
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            x -= 200 * Gdx.graphics.getDeltaTime();
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+
+        }
+
+        if (x < 0) {
+            x = 0;
+        }
+        if (x > 1820) {    // 1920-100
+            x = 1820;
+        }
+
+        if (y < 0) {
+            y = 0;
+        }
+
+        if (y > 930){
+            y =
+        }
 
     }
 
-    private void useSkill(){
+    private void useSkill() {
 
     }
 }
